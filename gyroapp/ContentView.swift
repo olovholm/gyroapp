bf2dee8b43fdf50a75da16fac5ca1847349e836b
Ola Lovholm <ola.lovholm@origo.oslo.kommune.no>
2025-03-09 19:36:38 +0100

    Initial Commit

---
+++ gyroapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gyroapp
+//
+//  Created by Ola Loevholm on 09/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gyroapp/gyroappApp.swift
@@ -0,0 +1,17 @@
+//
+//  gyroappApp.swift
+//  gyroapp
+//
+//  Created by Ola Loevholm on 09/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct gyroappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
